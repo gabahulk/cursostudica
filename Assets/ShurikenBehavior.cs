055064a8279b186d848a2fae23f33ed1e24859a2
Gabriel Fazzio de Paula <gabrielfpaula@gmail.com>
2018-12-05 23:21:03 -0200

    :tada: Initial commit!

---
+++ Assets/ShurikenBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenBehavior : MonoBehaviour {
+    public bool isGrounded = false;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            isGrounded = true;
+        }
+    }
+}
